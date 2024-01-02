cf575327892e5a0e5b06d4f7d8d98f4ab1c34c86
SonLPH1708 <112694749+SonLPH@users.noreply.github.com>
2024-01-02 11:38:25 +0700

    Move config into .env file

--- api/server.go
+++ api/server.go
@@ -8,18 +8,20 @@ import (
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
 
 	// add routes to the router
 	server.router = router
